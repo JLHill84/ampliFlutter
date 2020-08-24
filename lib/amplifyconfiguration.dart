82b8d148ac5d032980ef17551396494b3219033d
joshua.hill@slalom.com-at-580417612588 <joshua.hill@slalom.com>
2020-08-23 23:33:41 -0500

    firstsies

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,53 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "analytics": {
+        "plugins": {
+            "awsPinpointAnalyticsPlugin": {
+                "pinpointAnalytics": {
+                    "appId": "e78e4051072b4bb1aa97f5b5b2c95ef7",
+                    "region": "us-east-1"
+                },
+                "pinpointTargeting": {
+                    "region": "us-east-1"
+                },
+                "autoFlushEventsInterval": 10000
+            }
+        }
+    },
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:b96b0683-2d7c-4597-a90b-b7d528e0ea73",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                },
+                "PinpointAnalytics": {
+                    "Default": {
+                        "AppId": "e78e4051072b4bb1aa97f5b5b2c95ef7",
+                        "Region": "us-east-1"
+                    }
+                },
+                "PinpointTargeting": {
+                    "Default": {
+                        "Region": "us-east-1"
+                    }
+                }
+            }
+        }
+    }
+}''';
